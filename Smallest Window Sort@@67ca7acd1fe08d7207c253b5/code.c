b3db7ec0eb573dacdcca38d6a1c97f891230c478
Abhivansh Parashar <abhivanshparashar@gmail.com>
2025-03-28 23:26:21 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -3,26 +3,26 @@ int findUnsortedSubarray(int arr[], int n){
     int start = -1, end = -1;
     int i, j;
     for(int i=0; i<n-1; i++){
-        if(arr[i] >= arr[i+1]){
+        if(arr[i] > arr[i+1]){
             start = i;
             break;
         }
     }
-    if(start = -1){
+    if(start == -1){
         return 0;
     }
-    for(int j = 0; j<n-1; j++){
+    for(int i = 0; i<n-1; i++){
         if (arr[i] < arr[i+1]){
             end = j;
             break;
         }
     }
-    if(end = -1){
+    if(end == -1){
         return 0;
     }
     int min_val = arr[start];
     int max_val = arr[start];
-    for(int i = start+1; i<n-1; i++){
+    for(int i = start+1; i<end-1; i++){
         if(arr[i] < min_val){
             min_val = arr[i];
         }
@@ -31,13 +31,13 @@ int findUnsortedSubarray(int arr[], int n){
         }
     }
     for(int i = 0; i<start; i++){
-        if(arr[i] > start){
+        if(arr[i] > min_val){
             start = i;
             break;
         }
     }
     for(int i = end-1; i> start; i++){
-        if(arr[i] < end){
+        if(arr[i] < max_val){
             end = arr[i];
             break;
         }
