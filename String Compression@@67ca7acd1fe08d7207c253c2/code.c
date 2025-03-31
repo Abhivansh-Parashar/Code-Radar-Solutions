d98f59ea17cbc17294c828282145f6d1880afacc
Abhivansh Parashar <abhivanshparashar@gmail.com>
2025-03-31 21:23:46 +0530

    Solution to String Compression

--- String Compression@@67ca7acd1fe08d7207c253c2/code.c
+++ String Compression@@67ca7acd1fe08d7207c253c2/code.c
@@ -16,17 +16,18 @@ void compressString(char str[], char compressed[]) {
 
         compressed[compressedIndex++] = currentChar;
         if (count > 1) {
-            char countStr[20]; // Enough space for large counts
+            char countStr[20];
             sprintf(countStr, "%d", count);
             for (int j = 0; countStr[j] != '\0'; j++) {
                 compressed[compressedIndex++] = countStr[j];
             }
         }
     }
-    compressed[compressedIndex] = '\0'; // Null-terminate the compressed string
+    compressed[compressedIndex] = '\0';
 
-    // Compare lengths to see if compression was effective
-    if (compressedIndex < len) {
-        strcpy(str, compressed); // Copy compressed string back to str
+    // Compare lengths AFTER compression is complete
+    if (compressedIndex >= len) { // If compressed is longer or same, return original
+        strcpy(compressed, str); // Copy original string to compressed
     }
+    strcpy(str, compressed); // Copy compressed or original to str
 }
