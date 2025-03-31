c2cdbd19cac21002a2a125b7f4891168e7808db1
Abhivansh Parashar <abhivanshparashar@gmail.com>
2025-03-31 18:16:29 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -1,23 +1,23 @@
-// Your code here...// Your code here...
-# include <string.h>
-int selectionSort(char arr[][100], int n){
-    for(int i=0; i< n; i++){
-        int min_idx = i;
-        for(int j = i+1; j < n; j++){
-            if(arr[j] < arr[min_idx]){
-                min_idx = j;
+void selectionSortStrings(char arr[][100], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++) {
+            if (strcmp(arr[j], arr[min_index]) < 0) {
+                min_index = j;
             }
         }
-        if (min_idx != i) {
-            char temp[100]; // Temporary string for swapping
+        if (min_index != i) {
+            char temp[100];
             strcpy(temp, arr[i]);
-            strcpy(arr[i], arr[min_idx]);
-            strcpy(arr[min_idx], temp);
+            strcpy(arr[i], arr[min_index]);
+            strcpy(arr[min_index], temp);
         }
     }
 }
-int printArray(char arr[][100], int n){
-    for(int i=0; i< n; i++){
-        printf("%s \n", arr[i]);
+
+// Function to print an array of strings
+void printStringArray(char arr[][100], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", arr[i]);
     }
 }
